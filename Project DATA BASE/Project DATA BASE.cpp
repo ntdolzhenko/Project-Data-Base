b92838e472400a0c5276cc942fabd61386d2749e
ntdolzhenko <nt.dolzhenko@gmail.com>
2021-12-20 07:30:48 +0300

    .

--- Project DATA BASE/Project DATA BASE.cpp
+++ Project DATA BASE/Project DATA BASE.cpp
@@ -20,9 +20,12 @@ using namespace std;
 // КЛАССЫ:
 // 1. Таблица 
 //     Переменные:
+//         путь до файла с таблицей
 //         название таблицы
+//         количество строк
 //         список с названиями столбцов таблицы
 //         вектор со столбцами
+
 
 //     Методы: (public)
 //         создать (поля - 1, данные - 2)
@@ -42,28 +45,28 @@ using namespace std;
 
 //////////////////////////КЛАССЫ///////////////////////////////////
 
-// Столбец
+// СТОЛБЕЦ
 class Column {
 public:
-    string ColumnName;
+    string columnName;
     // int columnSize;
     vector <string> columnData;
-    int columnSize = columnData.size();
     // ссылка на другой столбец
+
+    // конструктор
+    Column(string columnName = "", vector <string> columnData = {}) {
+        this -> columnName = columnName;
+        this -> columnData = columnData;
+    }
 
 };
 
-
-
+// ТАБЛИЦА
 class Table{
 private:
-
-//    string tableName;
-//    vector <string> columnsNames;
-    // vector <Columns> columns;
-
-
+//
 public:
+    string pathToFile;
     string tableName;
     vector <string> columnsNames;
     vector <Column> tableBase;
@@ -87,7 +90,26 @@ public:
         cout << "1000000";
     };
     void showTheTable() {
-        cout << "1000000";
+        cout << "Name: " << tableName << endl;
+
+        //for (int i = 0; i < columnsNames.size(); i++) {
+         //   for (int j = 0; j < tableBase[0].columnData.size(); j++) cout << tableBase[i].columnData[j] << " ";
+          //  cout << endl;
+        //}
+
+        if (tableBase.size() != 0) {
+//            for (int i = 0; i < tableBase[0].columnData.size(); i++) {    // вылетает ошибка
+//               for (int j = 0; j < columnsNames.size(); j++) cout << tableBase[j].columnData[i] << " ";
+//                cout << endl;
+//            }
+            cout << "tableBase.size()    " << tableBase.size() << endl;
+            cout << "tableBase[0].columnData.size()            " << tableBase[0].columnData.size()<< endl;
+            for (int i = 0; i < tableBase[0].columnData.size(); i++) cout << tableBase[0].columnData[i] << " ";
+            //cout << "tableBase[0]    " << tableBase.size() << endl;
+
+            cout << "SUCCESSFULY";
+        }
+
     };
 
     void getGPA(string studentID, string subjectID) {
@@ -107,7 +129,8 @@ public:
     }
 
     // конструктор
-    Table(string tableName = "", vector <string> columnsNames = {}, vector <Column> tableBase = {}) {         // конструктор
+    Table(string pathToFile = "", string tableName = "", vector <string> columnsNames = {}, vector <Column> tableBase = { {} }) {         // конструктор
+        this -> pathToFile = pathToFile;
         this -> tableName = tableName;
         this -> columnsNames = columnsNames;
         this -> tableBase = tableBase;
@@ -121,9 +144,14 @@ public:
 
 /////////////////////////////////////////////////////////////////////////
 
-Table students = Table();
-Table courses = Table();
-Table grades = Table();
+Table students = Table("table_Students.txt", "Students");
+Table courses = Table("table_Courses.txt", "Courses");
+Table grades = Table("table_Grades.txt", "Grades");
+
+
+long long idsForStudents = 0;
+long long idsForCourses = 0;
+long long idsForGrades = 0;
 
 //////////////////////////////ФУНКЦИИ////////////////////////////////////
 
@@ -149,8 +177,63 @@ void getRequest() {
 
 //////////////////////////////ФУНКЦИИ////////////////////////////////////
 
-void readTheTables() {
+void splitTheString(vector<string>* list, string line , char separator = ' ') { // функция split по char для string
+    string s = "";
 
+    while (line.size() > 0) {
+        if (line[0] != separator) s += line[0];
+        else {
+            (*list).push_back(s);
+            s = "";
+        }
+        line.erase(0, 1);
+    }
+}
+
+bool readTheTables(Table* tableN) {
+/*
+* Name: Students
+* Data:
+* id    Last_Name    First_Name    Age    Class
+* 0      Dolzhenko    Anastasia     16       11
+* 1        Ivanov       Ivan        13       6       между данными везде один пробел(табуляция)
+* 2        Petrov       Petr        16       11
+*/
+    ifstream fin;
+    fin.open((*tableN).pathToFile);
+
+    if (!fin.is_open()) {
+        cout << "Eror ><. Please restart the programm." << endl;
+        fin.close();
+        return 0;
+    }
+
+    string line;
+    
+    fin >> line;
+    fin >> line;
+    fin >> line;
+    fin >> line;
+
+    //if (line == "Data:") fin >> line;
+
+    // здесь мы считали названия столбцов, их нужно записать в названия столбцов таблицы и в названия каждому столбцу
+    splitTheString(&((*tableN).columnsNames), line, ' ');
+    // создаем столбцы
+    for (int i = 0; i < ((*tableN).columnsNames).size(); i++) {
+        Column col = Column((*tableN).columnsNames[i]);
+        (*tableN).tableBase.push_back(col);
+    }
+
+    // считываем и записываем данные
+    while (!fin.eof()) {
+        //fin >> line;
+        getline(fin, line);
+        vector<string> dataGotten = {};
+        splitTheString(&dataGotten, line, ' ');
+        //for (int i = 0; i < dataGotten.size(); i++) cout << endl << dataGotten[i];
+        for (int i = 0; i < dataGotten.size(); i++) (*tableN).tableBase[i].columnData.push_back(dataGotten[i]);
+    }
 }
 
 void offerOptionsForTable(string tableN = "") {
@@ -172,7 +255,7 @@ void offerOptionsForTable(string tableN = "") {
     //cout << "\n6) Show the table."; // Показать таблицу.
 
 
-    /*
+    /* ВРЕМЕННО ЗАКОММЕНТИРОВАНО
     string request = "";
     cin >> request;
     if (request == "1") (*workingTable).createTheData();
@@ -188,7 +271,6 @@ void offerOptions() {
 
     cout << "\nPlease, choose what you would like to to do and input the number of chosen option (for example: 1).\n";
     cout << "\n1) Show table."; // Показать таблицу
-    //cout << "\n2) Show tables.";
     cout << "\n2) Get the student's GPA."; // получить средний балл ученика
     cout << "\n3) Change the table.\n"; // Изменить таблицу
 
@@ -232,7 +314,8 @@ void greeting() {
 void startOfProg() {
 
     // считываем таблицы из файлов
-    readTheTables();
+    readTheTables(&students);
+    cout << "CHECKING     " << students.tableBase.size() << endl;
     // приветствие
     greeting();
     // предлагаем выбор опций
