eac1b7c32e2aa3c45e0ee60c2b24aa953d9edf30
ntdolzhenko <nt.dolzhenko@gmail.com>
2021-12-20 04:15:31 +0300

    .

--- Project DATA BASE/Project DATA BASE.cpp
+++ Project DATA BASE/Project DATA BASE.cpp
@@ -1,20 +1,252 @@
-﻿// Project DATA BASE.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+//#include "Table.Cl.h"
 
-#include <iostream>
+using namespace std;
+
+// СТРУКТУРЫ: 
+// 1. Столбец
+//     Переменные:
+//         название
+//         (длина столбца)
+//         размер поля
+//         вектор с данными
+//         ссылка на другой похожий столбец в другой таблице
+//     (Методы)
+
+
+// КЛАССЫ:
+// 1. Таблица 
+//     Переменные:
+//         название таблицы
+//         список с названиями столбцов таблицы
+//         вектор со столбцами
+
+//     Методы: (public)
+//         создать (поля - 1, данные - 2)
+//         удалить (поля - 3, данные - 6)
+//         изменить (поля - 5, данные - 6)
+//         добавить столбец - 7
+//         удалить столбец - 8
+//         показать таблицу - 9
+//         показать средний балл ученика - 10
+//     Методы: (private)
+// ФУНКЦИИ:
+    // Запустить программу
+    // Получить request
+    // Запросить ввод задания и его выполнение
+    // Прекращение работы программы
+
+
+//////////////////////////КЛАССЫ///////////////////////////////////
+
+// Столбец
+class Column {
+public:
+    string ColumnName;
+    // int columnSize;
+    vector <string> columnData;
+    int columnSize = columnData.size();
+    // ссылка на другой столбец
+
+};
+
+
+
+class Table{
+private:
+
+//    string tableName;
+//    vector <string> columnsNames;
+    // vector <Columns> columns;
+
+
+public:
+    string tableName;
+    vector <string> columnsNames;
+    vector <Column> tableBase;
+
+    //void createTheBox() {};
+    void createTheData() { // создать новую строку
+        cout << "1000000";
+    };
+    //void deleteTheBox() {};
+    void deleteTheData() {
+        cout << "1000000";
+    };
+    //void changeTheBox() {};
+    void changeTheData() {
+        cout << "1000000";
+    };
+    void addTheColumn() {
+        cout << "1000000";
+    };
+    void deleteTheColumn() {
+        cout << "1000000";
+    };
+    void showTheTable() {
+        cout << "1000000";
+    };
+
+    void getGPA(string studentID, string subjectID) {
+        /*
+        int sumOtenka = 0; countOtenka = 0;
+
+        for (int i = 0; i < stolbezSize; i++) {
+            if (idYchenika[i] == nujniy_id_ychenika) {
+                if (idPredmeta[i] == nujniy_id_predmeta) {
+                    sumOtenka += otsenka;
+                    countOtsenka++;
+                }
+            }
+        }
+        int gpa =
+    */
+    }
+
+    // конструктор
+    Table(string tableName = "", vector <string> columnsNames = {}, vector <Column> tableBase = {}) {         // конструктор
+        this -> tableName = tableName;
+        this -> columnsNames = columnsNames;
+        this -> tableBase = tableBase;
+    } 
+
+
+    // void changeColumnName(){};
+    //void
+
+};
+
+/////////////////////////////////////////////////////////////////////////
+
+Table students = Table();
+Table courses = Table();
+Table grades = Table();
+
+//////////////////////////////ФУНКЦИИ////////////////////////////////////
+
+void getRequest() {
+    /* 
+    string request = "";
+     while(request != "Нет. Закрыть Базу Данных."){
+         cin >> request;
+         if(request == "1") createTheBox();
+         else if (request == "2") createTheData();
+         else if (request == "3") deleteTheBox();
+         else if (request == "4") deleteTheData();
+         else if (request == "5") changeTheBox();
+         else if (request == "6") changeTheData();
+         else if (request == "7") addTheColumn();
+         else if (request == "8") deleteTheColumn();
+         else if (request == "9") showTheTable();
+         else if (request == "10") GPA();
+     }
+     */
+}
+
+
+//////////////////////////////ФУНКЦИИ////////////////////////////////////
+
+void readTheTables() {
+
+}
+
+void offerOptionsForTable(string tableN = "") {
+    /* 
+    Table* workingTable;
+
+    if (tableN == "Students") workingTable = &students;
+    else if (tableN == "Courses") workingTable = &courses;
+    else if (tableN == "Grades") workingTable = &grades;
+    */
+
+    cout << "\nPlease, opt for the command you would like to perform and input the number of chosen option (for example: 1).\n";
+
+    cout << "\n1) Create or write the data."; // Записать данные
+    cout << "\n2) Delete the data."; // Удалить данные
+    cout << "\n3) Change the data."; // Изменить данные
+    cout << "\n4) Add the column."; // Добавить столбец
+    cout << "\n5) Delete the column.\n"; // Удалить столбец
+    //cout << "\n6) Show the table."; // Показать таблицу.
+
+
+    /*
+    string request = "";
+    cin >> request;
+    if (request == "1") (*workingTable).createTheData();
+    else if (request == "2") (*workingTable).deleteTheData();
+    else if (request == "3") (*workingTable).changeTheData();
+    else if (request == "4") (*workingTable).addTheColumn();
+    else if (request == "5") (*workingTable).deleteTheColumn();
+    //else if (request == "6") (*workingTable).showTheTable();
+    */
+}
+
+void offerOptions() {
+
+    cout << "\nPlease, choose what you would like to to do and input the number of chosen option (for example: 1).\n";
+    cout << "\n1) Show table."; // Показать таблицу
+    //cout << "\n2) Show tables.";
+    cout << "\n2) Get the student's GPA."; // получить средний балл ученика
+    cout << "\n3) Change the table.\n"; // Изменить таблицу
+
+    string request = "";
+    cin >> request;
+
+    if (request == "1") { // Показать таблицу
+        cout << "\nPlease, enter the table name: Students, Courses or Grades.\n";
+        string tableN = "";
+        cin >> tableN;
+        if (tableN == "Students") students.showTheTable();
+        else if (tableN == "Courses") courses.showTheTable();
+        else if (tableN == "Grades") grades.showTheTable();
+
+    }
+    else if (request == "2") { // получить средний балл ученика
+        cout << "\nPlease, enter student's ID and subject's ID separated by a space.\n";
+        string studentID = "", subjectID = "";
+        cin >> studentID >> subjectID;
+        grades.getGPA(studentID, subjectID);
+       // cout << "\nIf you want to exit, type down the code -1 (for example: -1).";
+    }
+    else if (request == "3") {   // Изменить таблицу
+        cout << "\nPlease, enter the table's name you would like to change:  Students, Courses or Grades.\n";
+        string tableN = "";
+        cin >> tableN;
+        offerOptionsForTable(tableN);
+//        if (tableN == "Students") students.showTheTable();
+//        else if (tableN == "Courses") courses.showTheTable();
+//        else if (tableN == "Grades") grades.showTheTable();
+    }
+}
+
+void greeting() {
+    //setlocale(LC_ALL, "Russian");
+    //cout << "Добро пожаловать в Базу Данных. Пожалуйста, выберите действие, которое Вы хотели бы выполнить и введите номер выбранного действия.";
+    //cout << "Welcome to the Data Base.\nPlease, opt for the option you would like to perform and input the number of chosen option (for example: 1).\n";
+    cout << "Welcome to the Data Base.\n";
+}
+
+void startOfProg() {
+
+    // считываем таблицы из файлов
+    readTheTables();
+    // приветствие
+    greeting();
+    // предлагаем выбор опций
+    offerOptions();
+
+}
+
+//////////////////////////////////////////////////////////////////
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    startOfProg();
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
